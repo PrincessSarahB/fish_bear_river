866d7841ddefffa646d04debcad1c343a6cd0791
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-04-03 15:20:30 +0100

    river name test written

--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -2,3 +2,18 @@ require("minitest/autorun")
 require("minitest/rg")
 
 require_relative("../river.rb")
+
+class RiverTest < MiniTest::Test
+
+  def setup()
+
+@river = River.new("Amazon", [])
+
+  end
+
+def test_river_name()
+
+assert_equal("Amazon", @river.river_name)
+
+end
+end
